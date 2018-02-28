86161c1b36cc9cd65e0932516659ece65037b3a8
abusada <faris.a@yamsafer.me>
2018-02-28 10:42:03 +0200

    test sync with codesandbox

--- src/Slides/TOC.js
+++ src/Slides/TOC.js
@@ -3,31 +3,12 @@ import Paper from "material-ui/Paper";
 import Typography from "material-ui/Typography";
 import ReactLogo from "../Components/Logo";
 
-var list = [
-  "What is React?",
-  "Passing Data Through Props",
-  "An Interactive Component",
-  "Lifting State Up",
-  "Functional Components"
-];
-
-var TableOfContent = function TableOfContent() {
-  return React.createElement(
-    "div",
-    {
-      style: {
-        textAlign: "left"
-      }
-    },
-    React.createElement("h1", null, "We will look into the following "),
-    React.createElement(
-      "ul",
-      null,
-      list.map(function(item, index) {
-        return React.createElement("li", { key: index }, item);
-      })
-    )
+function RaisedButtons(props) {
+  return (
+    <section>
+      <h1>Testing Sync with Codesandbox when pushing to github</h1>
+    </section>
   );
-};
+}
 
-export default TableOfContent;
+export default RaisedButtons;
