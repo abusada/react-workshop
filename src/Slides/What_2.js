c52e9e7ab952aa5c142b4997ae9cdbc0501ec15e
abusada <faris.a@yamsafer.me>
2018-03-04 20:50:44 +0200

    ...

--- src/Slides/What_2.js
+++ src/Slides/What_2.js
@@ -5,67 +5,67 @@ import SyntaxHighlighter from "react-syntax-highlighter";
 import { dark } from "react-syntax-highlighter/styles/hljs";
 import FinalResult from "../App";
 
+const Section = ({ title = "Title", children, variant = "display1" }) => (
+  <section style={{ marginBottom: 20 }}>
+    <Typography variant={variant}>{title}</Typography>
+    <Typography gutterBottom>{children}</Typography>
+  </section>
+);
+
+const Link = ({ href, children }) => (
+  <a target="_blank" rel="noopener noreferrer" href={href}>
+    {" "}
+    {children}{" "}
+  </a>
+);
+
+const CodeSnippets = {
+  "local-dev": `git clone git@github.com:abusada/react-workshop.git
+cd react-workshop
+npm install
+npm start
+`
+};
+
 function WhatWillWeDo(props) {
   return (
     <Grid container spacing={40}>
       <Grid item xs={12} sm={8}>
-        <Typography gutterBottom variant="display1">
-          What we're building?
-        </Typography>
-        <Typography gutterBottom>
+        <Section title="What we're building?">
           Together we will build a twitter like minimum viable product, you can
           login with your google account, tweet, and see tweets from other
           logged in users, You can see and play around with the final product on
           the right side.
-        </Typography>
-        <Typography gutterBottom />
-        <br />
-        <Typography gutterBottom variant="display1">
-          How to follow along?
-        </Typography>
-        <Typography gutterBottom>
+        </Section>
+        <Section title="How to follow along?">
           There are two ways to complete this tutorial: you could either write
           the code right in the browser, or you could set up a local development
           environment on your machine. You can choose either option depending on
           what you feel comfortable with.
-        </Typography>
-        <br />
-        <Typography gutterBottom variant="title">
-          If You Prefer to Write Code in the Browser
-        </Typography>
-        <Typography gutterBottom>
-          Open the following &nbsp;
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://codesandbox.io/s/github/abusada/react-workshop"
-          >
-            Link
-          </a>{" "}
-          &nbsp; And you're all set to start writing code 👍
-        </Typography>
-        <br />
-        <Typography gutterBottom variant="title">
-          If You Prefer to Write Code on your machine{" "}
-          <small>(not recommended for today 😅)</small>
-        </Typography>
-        <SyntaxHighlighter language="bash" style={dark}>
-          {`git clone git@github.com:abusada/react-workshop.git
-cd react-workshop
-npm install
-npm start`}
-        </SyntaxHighlighter>
-        <br />
-        <Typography gutterBottom variant="title">
-          You should now be all set to start writing code{" "}
-          <span role="img" aria-label="party" style={{ fontSize: "larger" }}>
-            🎉
-          </span>
-        </Typography>
+        </Section>
+        <Section
+          variant="title"
+          title="If You Prefer to Write Code in the Browser"
+        >
+          Open the following
+          <Link href="https://codesandbox.io/s/github/abusada/react-workshop">
+            link
+          </Link>
+          And you're all set to start writing code 👍
+        </Section>
+        <Section
+          variant="title"
+          title=" If You Prefer to Write Code on your machine"
+        >
+          <SyntaxHighlighter language="bash" style={dark}>
+            {CodeSnippets["local-dev"]}
+          </SyntaxHighlighter>
+          You should now be all set to start writing code 🎉
+        </Section>
       </Grid>
       <Grid item xs={12} sm={4}>
         <section style={{ overflowY: "visible", maxHeight: "90vh" }}>
-          <FinalResult />
+          {/* <FinalResult /> */}
         </section>
       </Grid>
     </Grid>
