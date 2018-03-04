6a595ea250481987772308f5f8562d33bbba3f44
Faris Abusada <faris.a@yamsafer.me>
2018-03-04 09:55:20 +0200

    prepare some imports for speed

--- src/App/Input/index.js
+++ src/App/Input/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Button from "material-ui/Button";
-
 import Input, { InputLabel, InputAdornment } from "material-ui/Input";
 import { FormControl, FormHelperText } from "material-ui/Form";
 
--- src/Training/Components/Input/index.js
+++ src/Training/Components/Input/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Button from "material-ui/Button";
+import Input, { InputLabel, InputAdornment } from "material-ui/Input";
+import { FormControl, FormHelperText } from "material-ui/Form";
 
 function TweetInput(props) {
   return <input type="text" placeholder="Tweet Input Field" />;
